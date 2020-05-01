46b0d09a18956f94b6eb6e8be64cfa9487e41791
Sean Jones <sean.d.jones92@gmail.com>
2020-05-01 14:58:40 -0600

    add semi colon

--- database/init.sh
+++ database/init.sh
@@ -8,7 +8,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         NAME            TEXT    NOT NULL,
         LASTUPDATED     DATE    NOT NULL,
         STATUS          INTEGER NOT NULL
-    )
+    );
     CREATE TABLE status(
         ID SERIAL PRIMARY KEY   NOT NULL,
         LONGSTAT        TEXT    NOT NULL,
